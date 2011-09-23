5876f17c34939fc4449c31d9251ba46c3ac0fd55
Kevin Qiu <kevin@freshbooks.com>
2011-09-22 21:07:09 -0400

    refactoring

--- src/greader.js
+++ src/greader.js
@@ -1,9 +1,21 @@
 (function($) {
+    var EntryAction = function(element) {
+        this.element = element;
+    };
+
+    EntryAction.prototype.addAction = function(name, action) {
+        this.element.append($("<span>").addClass("link unselectable").text(name).click(action));
+    };
+
     $("#entries").live('DOMNodeInserted', function(e) {
         if (!e.target.className.match(/entry\-actions/))
             return;
 
         var target = $(e.target);
-        target.append($("<span>").addClass("link unselectable").text("Twitter"));
+        var entryAction = new EntryAction(target);
+        entryAction.addAction('Twitter', function() {
+            alert('foo');
+        });
+
     });
 })(jQuery);
