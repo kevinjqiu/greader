386acac914bc91a387b09cbd640fed3c84f72282
Kevin Qiu <kevin@freshbooks.com>
2011-10-05 11:28:43 -0400

    regex callable isn't standard. switch to regex.exec()

--- src/greader.js
+++ src/greader.js
@@ -31,7 +31,7 @@
                 'name':'Click Through',
                 'fn':function(entry) {
                     chrome.extension.sendRequest({"type":"fetch_entry", "url":entry.url}, function(response) {
-                        var matched = /<div class="ldTitle">(.*?)<\/div>/(response.data);
+                        var matched = /<div class="ldTitle">(.*?)<\/div>/.exec(response.data);
                         var href = ($(matched[1]).attr("href"));
                         if (href !== null) {
                             chrome.extension.sendRequest({"type":"open_tab", "url":href}, function(response) {
